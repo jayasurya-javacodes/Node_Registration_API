335846a34528b837716ec3d2b0956f670f5e9ae6
jayasurya-javacodes <suryacse1806@gmail.com>
2026-06-17 16:26:22 +0530

    commit

---
+++ src/main/java/com/example/noderegistration/NodeRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.example.noderegistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NodeRegistrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NodeRegistrationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/noderegistration/NodeRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.noderegistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NodeRegistrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
